d1f1f9bd086a2af89658d28967cd48c1b88de036
hdub2 <mynameshunter@gmail.com>
2016-03-10 16:52:46 -0700

    google maps api implementation - displays map and allows addition of markers

--- app.js
+++ app.js
@@ -1,64 +1,28 @@
 (function(){
   angular.module( 'mapApp',[] )
-          .controller( 'mapController', ['$timeout', mapController] )
+          .controller( 'mapController', mapController )
 
-  function mapController($timeout){
+  function mapController(){
       var mc = this
       mc.markers = []
-      var map
-
-      function initMap(){
-        map = new google.maps.Map(document.getElementById('map'), {
+      mc.map = new google.maps.Map(document.getElementById('map'),
+      {
           center: {lat:36.889413, lng:-76.055226},
           zoom: 13
-        });
-      }
-
-      mc.mapMarker = function(){
-        // var newMarker = new Marker(event.pageX, event.pageY)
-        // mc.markers.push( newMarker )
-          console.log('debugging google api')
-
-        // ***** Working on Responsiveness - not crucial ***************
-        // $timeout( focusPrompt, 000)
-        //
-        // function focusPrompt (){
-        //   document.querySelector('.note-prompt').focus()
-        // }
-        // *************************************************************
-      }
-
-      mc.mapMarkerRemove = function(marker){
-        mc.markers.splice(mc.markers.indexOf(marker),1)
-      }
-
-      // *********************************************************************
-      // Marker Constructor - primary object pushed onto the state array:
-      // mc.markers[] -- exposes all of the methods needed for individual
-      // marker manipulation
-      // *********************************************************************
-      function Marker( x, y ){
-        this.top = y
-        this.left = x
-        this.note = ''
-        this.css = function(){
-          return "{'top': '" + this.top + "%','left': '" + this.left + "%'}"
-        }
-
-        this.noteVisible = false
-        this.promptVisible = true
-
-        this.showNote = function(){
-          this.noteVisible = true
-        }
-        this.hideNote = function(){
-          this.noteVisible = false
-        }
-        this.confirmNote = function(){
-          this.promptVisible = false
-        }
-      }
-  }
-
-
+      })
+      google.maps.event.addListener( mc.map, 'click', function(event){
+          var marker = new google.maps.Marker({
+            position : event.latLng,
+            map      : mc.map,
+            title    : "first marker!",
+            icon     :{
+              path  : google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+              scale : 10,
+              strokeColor : "CadetBlue",
+              strokeWeight: 4
+            }
+          })
+          mc.markers.push(marker)
+      })
+    }
 }())
