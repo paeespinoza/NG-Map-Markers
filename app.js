48d32510372fc7c3e09d38f35d36870f75791a52
hdub2 <mynameshunter@gmail.com>
2016-03-10 15:43:19 -0700

    refactored controller this to a variable: mc - slightly refactored CSS for clarity

--- app.js
+++ app.js
@@ -3,27 +3,40 @@
           .controller( 'mapController', ['$timeout', mapController] )
 
   function mapController($timeout){
-      this.markers = []
-      // this.showNoteInput = false
+      var mc = this
+      mc.markers = []
 
-
-      this.mapMarker = function(){
-        var newMarker = new Marker(event.pageX, event.pageY)
-        this.markers.push( newMarker )
+      mc.mapMarker = function(){
+        var newMarker = new Marker(
+          100* event.pageX/(document.querySelector('.map-wrapper').clientWidth),
+          100* event.pageY/(document.querySelector('.map-wrapper').clientHeight)
+        )
+        mc.markers.push( newMarker )
+        // *********************************************************************
+        // Working on Responsiveness - want input field to focus() on display
+        // has to wait until the DOM element exists.  Need to decide appropriate
+        // way to grab the input field. NON-ESSENTIAL
+        // *********************************************************************
         // $timeout( focusPrompt, 000)
         //
         // function focusPrompt (){
         //   document.querySelector('.note-prompt').focus()
         // }
+        // *********************************************************************
       }
 
-      this.mapMarkerRemove = function(marker){
-        this.markers.splice(this.markers.indexOf(marker),1)
+      mc.mapMarkerRemove = function(marker){
+        mc.markers.splice(mc.markers.indexOf(marker),1)
       }
 
+      // *********************************************************************
+      // Marker Constructor - primary object pushed onto the state array:
+      // mc.markers[] -- exposes all of the methods needed for individual
+      // marker manipulation
+      // *********************************************************************
       function Marker( x, y ){
-        this.top = y / (document.querySelector('.map-wrapper').clientHeight) *100
-        this.left = x / (document.querySelector('.map-wrapper').clientWidth) *100
+        this.top = y
+        this.left = x
         this.note = ''
         this.css = function(){
           return "{'top': '" + this.top + "%','left': '" + this.left + "%'}"
