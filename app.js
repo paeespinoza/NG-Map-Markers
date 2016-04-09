011adf90f4d4a84ece71b2421bd4098f3624820b
Hunter Williams <mynameshunter@gmail.com>
2016-04-09 14:59:56 -0600

    push before deleting local

---
+++ app.js
@@ -0,0 +1,28 @@
+(function(){
+  angular.module( 'mapApp',[] )
+          .controller( 'mapController', mapController )
+
+  function mapController(){
+      var mc = this
+      // mc.markers = []
+      mc.map = new google.maps.Map(document.getElementById('map'),
+      {
+          center: {lat:36.889413, lng:-76.055226},
+          zoom: 13
+      })
+      google.maps.event.addListener( mc.map, 'click', function(event){
+          var marker = new google.maps.Marker({
+            position : event.latLng,
+            map      : mc.map,
+            title    : "first marker!",
+            icon     :{
+              path  : google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+              scale : 10,
+              strokeColor : "CadetBlue",
+              strokeWeight: 4
+            }
+          })
+          mc.markers.push(marker)
+      })
+    }
+}())
